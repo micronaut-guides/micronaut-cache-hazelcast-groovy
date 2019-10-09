738d0698792f503e65966508d639b38e8563fd04
niravassar <assarn@objectcomputing.com>
2019-10-09 11:46:37 -0500

    cache put , invalidate example, with reset in between tests

--- complete/src/main/groovy/micronaut/cache/hazelcast/groovy/service/MessageService.groovy
+++ complete/src/main/groovy/micronaut/cache/hazelcast/groovy/service/MessageService.groovy
@@ -1,5 +1,7 @@
 package micronaut.cache.hazelcast.groovy.service
 
+import io.micronaut.cache.annotation.CacheInvalidate
+import io.micronaut.cache.annotation.CachePut
 import io.micronaut.cache.annotation.Cacheable
 
 import javax.inject.Singleton
@@ -12,6 +14,18 @@ class MessageService {
     @Cacheable("my-cache")
     String returnMessage(String message) {
         ++invocationCounter
-        message+"FromInsideMethod"
+        message+"_FromInsideMethodReturnMessage"
+    }
+
+    @CacheInvalidate(value="my-cache", all=true)
+    String invalidateAndReturnMessage(String message) {
+        ++invocationCounter
+        message+"_FromInsideMethodInvalidateAndReturnMessage"
+    }
+
+    @CachePut("my-cache")
+    String putReturnMessage(String message) {
+        ++invocationCounter
+        message+"_FromInsideMethodPutAndReturnMessage"
     }
 }
--- complete/src/test/groovy/micronaut/cache/hazelcast/groovy/service/MessageServiceSpec.groovy
+++ complete/src/test/groovy/micronaut/cache/hazelcast/groovy/service/MessageServiceSpec.groovy
@@ -14,57 +14,69 @@ class MessageServiceSpec extends Specification {
 
     static final String MESSAGE_TITLE = "myMessage"
 
-    @Inject MessageService messageService
-    @Inject DefaultCacheManager defaultCacheManager
+    @Inject
+    MessageService messageService
+    @Inject
+    DefaultCacheManager defaultCacheManager
 
-    def setup(){
+    def setup() {
         messageService.invocationCounter = 0
+        SyncCache<DefaultSyncCache> cache = defaultCacheManager.getCache("my-cache")
+        cache.invalidateAll()
     }
 
     def "test method cached after first invocation"() {
         when: 'called the first time'
         String message = messageService.returnMessage(MESSAGE_TITLE)
 
-        then: 'method runs by getting the gorm object'
-        message == "myMessageFromInsideMethod"
+        then: 'method runs by getting the counter increasing'
+        message == "myMessage_FromInsideMethodReturnMessage"
         messageService.invocationCounter == 1
 
         when: 'called a second time'
         message = messageService.returnMessage(MESSAGE_TITLE)
 
         then: 'the cache is accessed and the method doesnt run again'
-        message == "myMessageFromInsideMethod"
+        message == "myMessage_FromInsideMethodReturnMessage"
         messageService.invocationCounter == 1
 
         when: 'called again with a different param'
-        message = messageService.returnMessage(MESSAGE_TITLE+"Again")
+        message = messageService.returnMessage(MESSAGE_TITLE + "Again")
 
         then: 'method is invoked bc cache doesnt have the stored value'
-        message == "myMessageAgainFromInsideMethod"
+        message == "myMessageAgain_FromInsideMethodReturnMessage"
         messageService.invocationCounter == 2
     }
 
-    def "inspect the default cache manager"() {
-        when: 'use the manager to find the caches'
-        Set<String> cacheNames = defaultCacheManager.getCacheNames()
+    def "test method cache invalidate"() {
+        when: 'called the first time'
+        String message = messageService.returnMessage(MESSAGE_TITLE)
 
-        then:
-        cacheNames.size() == 1
-        cacheNames.asList()[0] == "my-cache"
+        then: 'method runs by getting the counter increasing'
+        message == "myMessage_FromInsideMethodReturnMessage"
+        messageService.invocationCounter == 1
 
-        when: 'get the cache'
-        SyncCache<DefaultSyncCache> cache = defaultCacheManager.getCache("my-cache")
-        String message = (String) cache.get("myMessage", String).get()
+        when: 'call a different method with @CacheInvalidate'
+        message = messageService.invalidateAndReturnMessage(MESSAGE_TITLE)
 
-        then:
-        cache.getName() == "my-cache"
-        message == "myMessageFromInsideMethod"
+        then: 'method is invoked because cache was invalidated and evicted'
+        message == "myMessage_FromInsideMethodInvalidateAndReturnMessage"
+        messageService.invocationCounter == 2
+    }
 
-        when: 'cache is invalidated'
-        cache.invalidateAll()
-        Optional<String> result = cache.get("myMessage", String)
+    def "test method cache put"() {
+        when: 'called the first time'
+        String message = messageService.returnMessage(MESSAGE_TITLE)
 
-        then:
-        !result.isPresent()
+        then: 'method runs by getting the counter increasing'
+        message == "myMessage_FromInsideMethodReturnMessage"
+        messageService.invocationCounter == 1
+
+        when: 'call a different method with @CachePut'
+        message = messageService.putReturnMessage(MESSAGE_TITLE)
+
+        then: 'method is invoked because cache it will put a new entry'
+        message == "myMessage_FromInsideMethodPutAndReturnMessage"
+        messageService.invocationCounter == 2
     }
 }
